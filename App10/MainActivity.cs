0a875e7090a5abf3402c0c910af46b1232c61dc8
lemontree <lemontreexf@aliyun.com>
2019-01-21 14:59:30 +0800

    demo for progressbar not shown issue

---
+++ App10/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace App10
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private LinearLayout overlayLayout;
+        private ProgressBar progressBar;
+        private Button switchBtn;
+        private bool isShown;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            // Instantiate the layout
+            overlayLayout =  FindViewById<LinearLayout>(Resource.Id.overlayLayout);
+            progressBar =  FindViewById<ProgressBar>(Resource.Id.overlayProgressBar);
+            switchBtn = FindViewById<Button>(Resource.Id.switchButton);
+
+            switchBtn.Click += SwitchBtn_Click;
+            overlayLayout.Visibility = Android.Views.ViewStates.Visible;
+            isShown = true;
+        }
+
+        private void SwitchBtn_Click(object sender, System.EventArgs e)
+        {
+            if (isShown)
+            {
+                overlayLayout.Visibility = Android.Views.ViewStates.Gone;
+                isShown = false;
+                switchBtn.Text = "Show";
+            }
+            else {
+                overlayLayout.Visibility = Android.Views.ViewStates.Visible;
+                isShown = true;
+                switchBtn.Text = "Hide";
+            }
+
+        }
+    }
+}
